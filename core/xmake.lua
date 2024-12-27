0f18a8bc0add077e7e014a531e0e44f4b6a4901c
A2va <49582555+A2va@users.noreply.github.com>
2024-12-27 18:06:18 +0100

    Install cosmocc in the actions

--- core/xmake.lua
+++ core/xmake.lua
@@ -50,8 +50,8 @@ end
 
 -- set cosmocc toolchain, e.g. xmake f -p linux --cosmocc=y
 if has_config("cosmocc") then
-    add_requires("cosmocc")
-    set_toolchains("@cosmocc")
+    -- add_requires("cosmocc")
+    -- set_toolchains("@cosmocc")
     set_policy("build.ccache", false)
 end
 
